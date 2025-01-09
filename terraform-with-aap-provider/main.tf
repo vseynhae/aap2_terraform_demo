ffce4d63188b86de56912ccbb0214eaf1057167c
Hicham Mourad <hicham.mourad@gmail.com>
2025-01-09 13:58:00 -0500

    update

--- terraform-with-aap-provider/main.tf
+++ terraform-with-aap-provider/main.tf
@@ -25,6 +25,8 @@ resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
 
 provider "aap" {
   host     = "https://localhost:8043"
+  username = "admin"          # CHANGE ME
+  password = "ansible123!"    # CHANGE ME
   insecure_skip_verify = true
 }
 
