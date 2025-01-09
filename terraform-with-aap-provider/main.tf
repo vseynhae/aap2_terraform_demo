73ca7266b1d8ec09cdad737837cdd980ca994c6c
Hicham Mourad <hicham.mourad@gmail.com>
2025-01-09 13:06:37 -0500

    updates

--- terraform-with-aap-provider/main.tf
+++ terraform-with-aap-provider/main.tf
@@ -5,11 +5,6 @@ terraform {
       version = "5.32.1"
     }
 
-    google = {
-      source = "hashicorp/google"
-      version = "5.12.0"
-    }
-
     aap = {
       source = "ansible/aap"
     }
@@ -28,30 +23,8 @@ resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
   }
 }
 
-provider "google" {
-  region = "northamerica-northeast1"
-}
-
-resource "google_compute_instance" "tf-demo-gcp-instance-2" {
-  name         = "tf-demo-gcp-instance-2"
-  machine_type = "e2-micro"
-  zone = "northamerica-northeast1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-11-bullseye-v20240110"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-}
-
 provider "aap" {
   host     = "https://localhost:8043"
-  username = "ansible"
-  password = "test123!"
   insecure_skip_verify = true
 }
 
@@ -60,11 +33,4 @@ resource "aap_host" "tf-demo-aws-ec2-instance-2" {
   name = "aws_instance_tf-demo-aws-ec2-instance-2"
   description = "An EC2 instance created by Terraform"
   variables = jsonencode(aws_instance.tf-demo-aws-ec2-instance-2)
-}
-
-resource "aap_host" "tf-demo-gcp-instance-2" {
-  inventory_id = 2
-  name = "google_compute_instance_tf-demo-gcp-instance-2"
-  description = "A GCE instance created by Terraform"
-  variables = jsonencode(google_compute_instance.tf-demo-gcp-instance-2)
 }
