a3f822dfae6bfe6e8b69486cb655ff740bd6a892
Hicham Mourad <hicham.mourad@gmail.com>
2025-01-22 16:02:08 -0500

    update

--- terraform-with-aap-provider/main.tf
+++ terraform-with-aap-provider/main.tf
@@ -16,6 +16,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Fetch the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "tf-demo-aws-ec2-instance-1" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
