e06fd7d93ea17cce5853eca471fcf1cb90ce0bdd
Hicham Mourad <hicham.mourad@gmail.com>
2025-01-24 11:12:30 -0500

    update

--- terraform-with-aap-provider/main.tf
+++ terraform-with-aap-provider/main.tf
@@ -21,14 +21,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "tf-demo-aws-ec2-instance-1" {
-  ami           = "ami-0005e0cfe09cc9050"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "tf-demo-aws-ec2-instance-1"
-  }
-}
-
 resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
