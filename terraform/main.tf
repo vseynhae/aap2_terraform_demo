e1cb1d6f7495cd9d42ca60d26acf2f2e07f60ef1
Sebastien Wains <sebw@users.noreply.github.com>
2025-05-26 16:45:04 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
