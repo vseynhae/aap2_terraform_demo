b0f54dfb0fdfda093845ba7f61be34ff74abc5ae
Sebastien Wains <sebw@users.noreply.github.com>
2025-05-26 16:40:54 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,15 +14,59 @@ provider "aws" {
 }
 
 # Fetch the default VPC
-data "aws_vpc" "default" {
-  default = true
+#data "aws_vpc" "default" {
+#  default = true
+#}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
-resource "aws_instance" "tf-demo-aws-ec2-instance-1" {
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_instance" "voxxed1" {
   ami           = "ami-00513967e6b47e386"
   instance_type = "t3.medium"
   tags = {
-    Name = "tf-demo-aws-ec2-instance-1"
+    Name = "voxxed1"
   }
 
 }
